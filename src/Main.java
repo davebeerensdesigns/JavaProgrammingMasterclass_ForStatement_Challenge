3cc74b36739377f321b81f78ca9102c74d9456d3
davebeerensdesigns <84903154+davebeerensdesigns@users.noreply.github.com>
2023-08-29 15:52:44 +0200

    answer

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,26 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int primeCounter = 0;
+        for(int i = 50; i <= 1000; i++){
+            if(isPrime(i)){
+                System.out.println(i + " is a prime number");
+                primeCounter++;
+            }
+            if(primeCounter == 3){
+                break;
+            }
+        }
+    }
+
+    public static boolean isPrime(int number){
+        if(number <= 1){
+            return false;
+        }
+        for (int i = 2; i <= number / 2; i++){
+            if ((number % i) == 0) {
+                return false;
+            }
+        }
+        return true;
     }
 }
